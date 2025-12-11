3b4d6119b81c70837c5c6523791d19ae967aca34
SomaPrasanth <somaprasanthr@gmail.com>
2025-12-11 11:21:03 +0530

    Prepare for deployment: Added env var support

--- database.py
+++ database.py
@@ -1,19 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import urllib.parse
+import urllib.parse 
 
-raw_password = "Somu@2006"
-encoded_password = urllib.parse.quote_plus(raw_password)
+# 1. Try to get the Cloud DB URL from Environment Variable
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# FORMAT: postgresql://user:password@localhost/dbname
-DATABASE_URL = f"postgresql://postgres:{encoded_password}@localhost/command_gateway"
+# 2. If no cloud DB, use local fallback (Your hardcoded one)
+if not DATABASE_URL:
+    raw_password = "Somu@2006" 
+    encoded_password = urllib.parse.quote_plus(raw_password)
+    DATABASE_URL = f"postgresql://postgres:{encoded_password}@localhost:5432/command_gateway"
+else:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency to get DB session in endpoints
 def get_db():
     db = SessionLocal()
     try:
